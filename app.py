b5963c2615c9f6eb94015b20ab0cb408ec10bb0b
Matheus Boscariol <moboscariol2010@gmail.com>
2025-10-22 18:24:53 -0400

    added app.py

---
+++ app.py
@@ -0,0 +1,8 @@
+"""
+This project is licensed under the GNU Afferi General Public License v3.0.
+You may obtain a copy of the license at:
+
+    https://www.gnu.org/licenses/agpl-3.0.html
+
+© 2025 Matheus
+"""
